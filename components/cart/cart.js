48d5b70c42079498bc7009f70ea128fbd6ab2208
mayur.gupta <mayur.gupta@tothenew.com>
2016-11-04 16:43:32 +0530

    cart files

---
+++ components/cart/cart.js
@@ -0,0 +1,61 @@
+angular.module("cart", []).factory("cart", function() {
+	var cartData = [];
+	return {
+		addProduct : function(id, name, price) {
+			var addedToExistingItem = false;
+			for (var i = 0; i < cartData.length; i++) {
+				if (cartData[i].id == id) {
+					cartData[i].count++;
+					addedToExistingItem = true;
+					break;
+				}
+			}
+			if (!addedToExistingItem) {
+				cartData.push({
+					count : 1,
+					id : id,
+					price : price,
+					name : name
+				});
+			}
+		},
+		removeProduct : function(id) {
+			for (var i = 0; i < cartData.length; i++) {
+				if (cartData[i].id == id) {
+					if (cartData[i].count > 1) {
+						cartData[i].count--;
+					} else {
+						cartData.splice(i, 1);
+					}
+
+					break;
+				}
+			}
+		},
+		getProducts : function() {
+			return cartData;
+		}
+	};
+}).directive("cartsummary", function(cart) {
+	return {
+		restrict : "AEC",
+		templateUrl : "components/cart/cartSummary.html",
+		controller : function($scope) {
+			var cartData = cart.getProducts();
+			$scope.total = function() {
+				var total = 0;
+				for (var i = 0; i < cartData.length; i++) {
+					total += (cartData[i].price * cartData[i].count);
+				}
+				return total;
+			};
+			$scope.itemCount = function() {
+				var total = 0;
+				for (var i = 0; i < cartData.length; i++) {
+					total += cartData[i].count;
+				}
+				return total;
+			};
+		}
+	};
+});
